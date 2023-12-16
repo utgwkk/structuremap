50a1e81a2fda879ee8fe0d6312558399d25228c8
utgwkk <utagawakiki@gmail.com>
2023-12-16 22:29:35 +0900

    encode nil to nil map

--- structuremap.go
+++ structuremap.go
@@ -1,6 +1,13 @@
 package structuremap
 
+import "reflect"
+
 func Encode(val any) (map[string]any, error) {
+	v := reflect.ValueOf(val)
+	if v.IsNil() {
+		return nil, nil
+	}
+
 	// TODO: implement it
 	return nil, nil
 }
