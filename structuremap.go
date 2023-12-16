cdf98e2ec69312cf5b3cebd1a029423d51d84e54
utgwkk <utagawakiki@gmail.com>
2023-12-16 22:39:07 +0900

    recursively dereference and validate type

--- structuremap.go
+++ structuremap.go
@@ -1,11 +1,22 @@
 package structuremap
 
-import "reflect"
+import (
+	"fmt"
+	"reflect"
+)
 
 func Encode(val any) (map[string]any, error) {
 	v := reflect.ValueOf(val)
-	if v.IsNil() {
+
+	// recursively dereference
+	for v.Kind() == reflect.Pointer {
+		v = v.Elem()
+	}
+	if !v.IsValid() {
 		return nil, nil
+	}
+	if v.Kind() != reflect.Struct {
+		return nil, fmt.Errorf("not a struct type (%T)", val)
 	}
 
 	// TODO: implement it
--- structuremap_test.go
+++ structuremap_test.go
@@ -14,7 +14,7 @@ func TestEncodeSuccess(t *testing.T) {
 	}{
 		{
 			name: "nil",
-			input: (*struct{
+			input: (*struct {
 				A string
 			})(nil),
 			want: nil,
@@ -29,6 +29,37 @@ func TestEncodeSuccess(t *testing.T) {
 			}
 			if diff := cmp.Diff(tt.want, got); diff != "" {
 				t.Errorf("Value mismatch (-want +got):\n%s", diff)
+			}
+		})
+	}
+}
+
+func ptrOf[T any](t T) *T { return &t }
+
+func TestEncodeFailure(t *testing.T) {
+	testcases := []struct {
+		name  string
+		input any
+	}{
+		{
+			name:  "string",
+			input: "aaa",
+		},
+		{
+			name:  "int",
+			input: 100,
+		},
+		{
+			name: "string pointer",
+			input: ptrOf("aaa"),
+		},
+	}
+	for _, tt := range testcases {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			_, err := Encode(tt.input)
+			if err == nil {
+				t.Error("expected an error")
 			}
 		})
 	}
